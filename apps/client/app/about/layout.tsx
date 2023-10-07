4224851dc2899a1d836b2c3e559cafcc0e72d7ba
piyawat.j <piyawat.j@merudy.com>
2023-10-07 15:39:46 +0700

    updated: package file

---
+++ apps/client/app/about/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <nav>About Layout</nav>
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ apps/client/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
+export default function Post() {
+  return (
+    <div>
+      Post
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+}
